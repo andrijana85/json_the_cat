77a15b49ba5d20b80f2dbeebcfdaaef27c835a34
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-16 18:35:14 -0400

    implementing breedFetcher.js

---
+++ breedFetcher.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const breed = process.argv.slice(2);
+// fetch the data from the API endpoint using request
+request.get(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`,(error, response, body) => {
+  if (error)
+    return console.log('ERROR:', error);
+  if (body) {
+    // use JSON.parse to convert the JSON string into an actual object
+    const data = JSON.parse(body);
+    console.log(data);
+    // console.log(typeof data);
+    // access the first entry in the data array and print out the description
+    const firstBreed = data[0];
+    console.log(firstBreed.description);
+
+    if (data.length === 0) {
+      console.log(`Sorry.The requested breed ${breed} is not found`);
+    }
+  }
+
+});
